8046bcff53cfac815ce9d8a9d856629a3f2961e1
Alexander Korataev <alexandkorataev@gmail.com>
2022-09-08 17:09:11 +0700

    Update app.js

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -2,25 +2,6 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#ffffff";
-tg.MainButton.color = "#2cab37";
-
-let item = "";
-
 let espresso = document.getElementById("espresso");
 
-espresso.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-        console.log("CLICK!!");
-    }
-    else {
-        tg.MainButton.setText("Espresso");
-        item = "espresso";
-        tg.MainButton.show();
-    }
-});
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(item);
-});
+espresso.addEventListener("click", tg.sendData("espresso"));
